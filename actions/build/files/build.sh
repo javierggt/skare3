9115c58bd1de69e38eed49354a4f1baab4c8ce90
Javier Gonzalez <javierggt@yahoo.com>
2020-01-17 18:44:00 -0500

    refactor conda auth process

--- actions/build/files/build.sh
+++ actions/build/files/build.sh
@@ -5,6 +5,9 @@ workdir=`pwd`
 echo "Building $package"
 echo "GIT_USERNAME: ${GIT_USERNAME}"
 
+# this puts the password (from the environment) in .condarc so dummy conda can use it
+sed -e "s/\${CONDA_PASSWORD}/${CONDA_PASSWORD}/g" .condarc.in > .condarc
+
 # If docker user has a different ID than the github user, permissions would not be properly set
 #sudo chown -R ska $workdir
 
